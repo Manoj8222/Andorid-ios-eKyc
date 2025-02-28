31db1a8e362631d6101c45cf3b9115a08d2844f9
Innovitegra Solutions <innovitegrasolutions@Innovitegras-MacBook-Air-2.local>
2025-02-28 11:16:05 +0530

    android update

--- android/src/main/java/com/inno/ReactNativeActivity.kt
+++ android/src/main/java/com/inno/ReactNativeActivity.kt
@@ -11,7 +11,7 @@ class ReactNativeActivity : ReactActivity() {
         Log.d("ReactNativeActivity", "onCreate called")
     }
 
-    override fun getMainComponentName(): String = "InnoExample"
+    override fun getMainComponentName(): String = "ReactNativeInnoTrust"
 
     override fun createReactActivityDelegate(): ReactActivityDelegate {
         return object : ReactActivityDelegate(this, mainComponentName) {
